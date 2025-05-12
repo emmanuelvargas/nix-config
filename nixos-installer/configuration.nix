69f32b1ff72999c6024a4f25c81abad8c2a6f0f4
Emmanuel Vargas <emmanuel.vargas@gmail.com>
2025-05-12 16:23:06 +0200

    fix: update OpenSSH settings and default gateway in configuration

--- nixos-installer/configuration.nix
+++ nixos-installer/configuration.nix
@@ -2,6 +2,18 @@
   # ssh-agent is used to pull my private secrets repo from github when deploying my nixos config.
   programs.ssh.startAgent = true;
 
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    settings = {
+      X11Forwarding = true;
+      # root user is used for remote deployment, so we need to allow it
+      PermitRootLogin = "prohibit-password";
+      PasswordAuthentication = true; # disable password login
+    };
+    openFirewall = true;
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -16,7 +28,7 @@
   networking = {
     # configures the network interface(include wireless) via `nmcli` & `nmtui`
     networkmanager.enable = true;
-    defaultGateway = "192.168.122.100";
+    defaultGateway = "192.168.0.254";
   };
   system.stateVersion = "24.11";
 }

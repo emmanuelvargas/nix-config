d3f15276d5140f2a7447b6085b53197267ee7323
Emmanuel Vargas <emmanuel.vargas@gmail.com>
2025-05-12 17:45:17 +0200

    fix: update hostname in default.nix to match server name

--- hosts/nixosryanai/default.nix
+++ hosts/nixosryanai/default.nix
@@ -5,7 +5,7 @@
 #
 #############################################################
 let
-  hostName = "ai"; # Define your hostname.
+  hostName = "nixosryanai"; # Define your hostname.
 in {
   imports = [
     ./netdev-mount.nix

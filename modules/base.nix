f71dac1ddbe5f6c593741e56717889949838961f
Emmanuel Vargas <emmanuel.vargas@gmail.com>
2025-05-12 15:14:30 +0200

    remove age for now

--- modules/base.nix
+++ modules/base.nix
@@ -117,7 +117,7 @@
     builders-use-substitutes = true;
   };
 
-  nix.extraOptions = ''
-    !include ${config.age.secrets.nix-access-tokens.path}
-  '';
+  # nix.extraOptions = ''
+  #   !include ${config.age.secrets.nix-access-tokens.path}
+  # '';
 }

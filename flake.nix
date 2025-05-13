b02b892e849f89b561e79fcee2c84239edec8251
Emmanuel Vargas <emmanuel.vargas@gmail.com>
2025-05-13 13:54:57 +0200

    fix: correct repository URL for nix-secrets in flake.nix

--- flake.nix
+++ flake.nix
@@ -133,7 +133,7 @@
     # my private secrets, it's a private repository, you need to replace it with your own.
     # use ssh protocol to authenticate via ssh-agent/ssh-key, and shallow clone to save time
     mysecrets = {
-      url = "git+ssh://git@github.com/emmanuelvargas/nix-secrets.git?shallow=1";
+      url = "git+ssh://git@github.com/emmanuelvargas/nix-secret.git?shallow=1";
       flake = false;
     };
 

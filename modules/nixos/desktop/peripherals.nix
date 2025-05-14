d4efd5a007e780824c060d22c8a2d03c49643ce3
Emmanuel Vargas <emmanuel.vargas@gmail.com>
2025-05-14 12:00:40 +0200

    fix: comment out keyd configuration in peripherals.nix

--- modules/nixos/desktop/peripherals.nix
+++ modules/nixos/desktop/peripherals.nix
@@ -60,15 +60,15 @@
 
     # A key remapping daemon for linux.
     # https://github.com/rvaiya/keyd
-    keyd = {
-      enable = true;
-      keyboards.default.settings = {
-        main = {
-          # overloads the capslock key to function as both escape (when tapped) and control (when held)
-          capslock = "overload(control, esc)";
-          esc = "capslock";
-        };
-      };
-    };
+    # keyd = {
+    #   enable = true;
+    #   keyboards.default.settings = {
+    #     main = {
+    #       # overloads the capslock key to function as both escape (when tapped) and control (when held)
+    #       capslock = "overload(control, esc)";
+    #       esc = "capslock";
+    #     };
+    #   };
+    # };
   };
 }

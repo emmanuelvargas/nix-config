8a0dfa537a2c12e198de28979f4dd3407a1c4274
Emmanuel Vargas <emmanuel.vargas@gmail.com>
2025-05-14 09:50:36 +0200

    Remove calculator translator and related configurations
    - Deleted the calculator_translator.lua script which provided a simple calculator functionality using Lua expressions.
    - Removed references to the calculator translator in rime.lua.
    - Deleted squirrel.custom.yaml and squirrel.yaml configuration files, which contained custom settings and color schemes for the Squirrel input method.

--- hosts/nixosryanai/home.nix
+++ hosts/nixosryanai/home.nix
@@ -18,7 +18,7 @@
     enable = true;
     extraConfig = ''
       Host github.com
-          IdentityFile ~/.ssh/idols-ai
+          IdentityFile ~/.ssh/nixosryanai
           # Specifies that ssh should only use the identity file explicitly configured above
           # required to prevent sending default identity files first.
           IdentitiesOnly yes
